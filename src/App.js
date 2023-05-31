8996606415357f6ddf5a12fbcf9281fc0f87093c
Alena Abharian <alenar2531@gmail.com>
2023-05-31 00:34:30 -0700

    components

--- src/App.js
+++ src/App.js
@@ -19,15 +19,10 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        <PizzaBlock/>
-                        <PizzaBlock/>
-                        <PizzaBlock/>
-                        <PizzaBlock/>
-                        <PizzaBlock/>
-                        <PizzaBlock/>
-                        <PizzaBlock/>
-                        <PizzaBlock/>
-                        <PizzaBlock/>
+                        <PizzaBlock title="Мексиканская" price= {500}/>
+                        <PizzaBlock title="Мексиканская" price={500}/>
+                        <PizzaBlock title="Мексиканская" price={500}/>
+                        <PizzaBlock title="Мексиканская" price={500}/>
                     </div>
                 </div>
             </div>
