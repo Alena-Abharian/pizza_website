410a24887ca163d9ab4550a7a38d128282240b80
Alena Abharian <alenar2531@gmail.com>
2023-06-03 15:43:38 -0700

    backend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
-import pizzas from './assets/pizzas.json';
-
 import './scss/app.scss';
 
 
 function App() {
+    const [items, setItems] =useState([]);
+
+   useEffect(() => {
+       fetch('https://647bb7bad2e5b6101db18d30.mockapi.io/item')
+           .then(res => res.json())
+           .then((json) => {
+               setItems(json)
+           })
+   }, [])
+
+
     return (
         <div className="wrapper">
             <Header/>
@@ -21,7 +30,7 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map((obj) =>
+                        {items.map((obj) =>
                             <PizzaBlock key={obj.id} {...obj}/>)}
                     </div>
                 </div>
