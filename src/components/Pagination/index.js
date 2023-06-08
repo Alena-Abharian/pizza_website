b34f74866e02203a052c765489c374e370f9a018
Alena Abharian <alenar2531@gmail.com>
2023-06-08 16:01:07 -0700

    pagination

---
+++ src/components/Pagination/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import ReactPaginate from 'react-paginate';
+
+import style from './Pagination.module.scss'
+
+const Pagination = ({onChangePage}) => {
+    return (
+        <>
+            <ReactPaginate
+                className={style.root}
+                breakLabel="..."
+                nextLabel=">"
+                previousLabel="<"
+                onPageChange={e => onChangePage(e.selected + 1)}
+                pageRangeDisplayed={4}
+                pageCount={3}
+                renderOnZeroPageCount={null}
+            />
+        </>
+    );
+};
+
+export default Pagination;
